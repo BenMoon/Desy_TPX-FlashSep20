54c1c2e63601b1910cd8af88479707c4f6ef1a39
Hubertus Bromberger <hubertus.bromberger@cfel.de>
2020-09-25 16:13:06 +0200

    Update on timeresolution

--- timewalk_fun.py
+++ timewalk_fun.py
@@ -5,6 +5,19 @@ from sklearn.cluster import DBSCAN
 from scipy.optimize import curve_fit
 from scipy.stats import norm
 
+def getData(fname):
+    with h5py.File(fname, 'r') as f:
+        rawNr  = f['raw/trigger nr'][:]
+        rawTof = f['raw/tof'][:]*1e6
+        rawTot = f['raw/tot'][:]
+        rawX   = f['raw/x'][:]
+        rawY   = f['raw/y'][:]
+        centNr = f['centroided/trigger nr'][:]
+        centTof= f['centroided/tof'][:]*1e6
+        centTot= f['centroided/tot max'][:]
+        centY  = f['centroided/y'][:]
+        centX  = f['centroided/x'][:]
+    return rawNr, rawTof, rawTot, rawX, rawY, centNr, centTof, centTot, centY, centX
 
 def gauss(x, *p):
     A, mu, sigma = p
@@ -52,7 +65,7 @@ def plot_TofTot_hv(tof, tot, region, fname, **kwargs):
 
     #plt.imshow(time_hist.T, origin='lower', cmap='jet')
     return hv.Image(time_hist.T[::-1], bounds=(tot_bins[0], time_bins[0], tot_bins[-1], time_bins[-1])).opts(
-        width=1000, cmap='jet', title=fname, ylabel='diff TOF', xlabel='TOT', logz=True)
+        width=1000, cmap='jet', title=fname, ylabel='diff TOF', xlabel='TOT', clim=(0.1, None), logz=True)
 
 def compute_timewalk(tof, tot, region, maxTot_slice):
     tot_points = []
@@ -102,7 +115,7 @@ def compute_timewalk(tof, tot, region, maxTot_slice):
         tot_points.append(tot_bins[b])
 
     image = hv.Image(time_hist.T[::-1], bounds=(tot_bins[0], time_bins[0], tot_bins[-1], time_bins[-1])).opts(
-        width=1200, height=300, cmap='jet', ylabel='diff TOF', xlabel='TOT', logz=True, tools=['hover'])
+        width=1200, height=300, cmap='jet', ylabel='diff TOF', xlabel='TOT', clim=(0.1, None), logz=True, tools=['hover'])
     return np.array(tot_points), np.array(time_walk_points), image
 
 def compute_tw_lookup(tof, tot, region, maxTot_slice=167, minTot=0, maxToT_1=4000, polyorder=12):
@@ -135,13 +148,56 @@ def compute_tw_lookup(tof, tot, region, maxTot_slice=167, minTot=0, maxToT_1=400
     c = hv.Curve((x2, tw_lookup2)).opts(color='green', line_width=1)
     return hist * a * b * c, tot_lookup_table * 1E-9
 
-def doCentroiding(shot, x, y, tof, tot, _epsilon=2, _samples=5):    
+def doCentroiding_old(shot, x, y, tof, tot, _epsilon=2, _samples=5):    
     tof_eps = 81920 * (25. / 4096)# * 1E-9
 
     tof_scale = _epsilon / tof_eps
     X = np.vstack((shot * _epsilon * 1000, x, y, tof * tof_scale)).transpose()
     dist = DBSCAN(eps=_epsilon, min_samples=_samples, metric='euclidean', n_jobs=15).fit(X)
     labels = dist.labels_ + 1
+
+    label_filter = labels != 0 # ignore noise
+
+    def cluster_properties(shot, x, y, tof, tot, labels):
+        import scipy.ndimage as nd
+
+        label_index = np.unique(labels)
+        tot_max = np.array(nd.maximum_position(tot, labels=labels, index=label_index)).flatten()
+        # tof_min = np.array(nd.minimum_position(tof,labels=labels,index=label_index)).flatten()
+        # print(tot_max)
+        tot_sum = nd.sum(tot, labels=labels, index=label_index)
+        cluster_x = np.array(nd.sum(x * tot, labels=labels, index=label_index) / tot_sum).flatten()
+        cluster_y = np.array(nd.sum(y * tot, labels=labels, index=label_index) / tot_sum).flatten()
+        cluster_tof = np.array(nd.sum(tof * tot, labels=labels, index=label_index) / tot_sum).flatten() # timewalk
+        cluster_tot = tot[tot_max]
+        #cluster_tof = tof[tot_max] # no timewalk
+        cluster_shot = shot[tot_max]
+
+        return cluster_shot, cluster_x, cluster_y, cluster_tof, cluster_tot
+
+    props = cluster_properties(shot[label_filter], x[label_filter], y[label_filter], tof[label_filter],
+                                                tot[label_filter], labels[label_filter])
+
+    XCentNew2  = pd.DataFrame(np.column_stack((props[0], props[1], props[2], props[3], props[4])), 
+                 columns=['nr', 'x', 'y', 'tof', 'tot'])
+    return XCentNew2
+
+
+def doCentroiding(shot, x, y, tof, tot, _epsilon=2, _samples=5):    
+    tof_eps = 81920 * (25. / 4096)# * 1E-9
+
+    tof_scale = _epsilon / tof_eps
+    for idx in range(0, int(shot.max())+1, 75):
+        mask = np.logical_and(shot >= idx, shot < idx+75)
+        X = np.vstack((shot[mask] * _epsilon * 1000, x[mask], y[mask], tof[mask] * tof_scale)).transpose()
+        dist = DBSCAN(eps=_epsilon, min_samples=_samples, metric='euclidean', n_jobs=15).fit(X)
+        new_labels = dist.labels_ + 1
+        noise_idx = np.where(new_labels == 0)[0]
+        new_labels += label_counter
+        new_labels[noise_idx] = 0
+        labels.append(new_labels)
+        label_counter += new_labels.max()
+    labels = np.concatenate(labels) + 1
 
     label_filter = labels != 0 # ignore noise
 
