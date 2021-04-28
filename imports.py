6abef9cd1324026b042fd3865486994843849081
Hubertus <hubertus.bromberger@cfel.de>
2021-04-28 21:40:14 +0200

    Add background, datathief data

--- imports.py
+++ imports.py
@@ -15,11 +15,13 @@ hv.extension('bokeh', 'matplotlib')
 from bokeh.io import export_png, export_svgs
 
 
-opts.defaults(opts.Scatter(width=1000, height=300),
-              opts.Histogram(width=1000, height=300),
-              opts.Image(width=1000, height=300),
-              opts.Curve(width=1000, height=300),
-              opts.Points(width=1000, height=300))
+opts.defaults(
+    opts.Scatter(width=1000, height=300, tools=['hover']),
+    opts.Histogram(width=1000, height=300, tools=['hover']),
+    opts.Image(width=1000, height=300, tools=['hover']),
+    opts.Curve(width=1000, height=300, tools=['hover']),
+    opts.Points(width=1000, height=300, tools=['hover']),
+)
 
 
 %pylab inline
