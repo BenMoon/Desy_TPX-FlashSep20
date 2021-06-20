7261b3d0fb426c5a350c3a7bfa235f620ba49a34
BenMoon <hubertus.bromberger@cfel.de>
2021-06-20 21:50:49 +0200

    Update

--- imports.py
+++ imports.py
@@ -19,7 +19,7 @@ from bokeh.io import export_png, export_svgs
 opts.defaults(
     opts.Scatter(width=1000, height=300, tools=["hover"]),
     opts.Histogram(width=1000, height=300, tools=["hover"]),
-    opts.Image(width=1000, height=300, tools=["hover"]),
+    opts.Image(width=400, height=400, tools=["hover"], cmap='jet'),
     opts.Curve(width=1000, height=300, tools=["hover"]),
     opts.Points(width=1000, height=300, tools=["hover"]),
 )
